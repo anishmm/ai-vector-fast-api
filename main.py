bd4f566767aa2f930826ce61362b21462845c2ba
anish <anish.mm@saffrontech.net>
2025-04-30 15:02:41 +0530

    update 13

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import os
+import logging
 from typing import Optional,List, Dict, Any
 from fastapi import FastAPI
 from pydantic import BaseModel
 from langchain_community.vectorstores.faiss import FAISS
 from dotenv import load_dotenv, find_dotenv
-
+from contextlib import asynccontextmanager
 
 load_dotenv()
 
-
+logger = logging.getLogger('uvicorn.error')
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 print(GROQ_API_KEY)
 
@@ -31,11 +32,26 @@ embeddings = None
 llm = None
 databases: Dict[str, FAISS] = {}
 
-app = FastAPI()
+@asynccontextmanager
+async def initialize_models(app: FastAPI):
+    global embeddings, llm, databases
+    try:
+        logger.info('START')
+        yield
+        logger.info('END')
+        return True
+    except Exception as e:
+        print(f"Initialization error: {e}")
+        embeddings = None
+        llm = None
+        return False
+
+app = FastAPI(lifespan=initialize_models)
 
 
 @app.get("/")
 async def root():
+    logger.info('GET /')
     return {"message": "Hello World"}
 
 @app.get("/items/{item_id}")
@@ -46,6 +62,7 @@ def read_item(item_id: int, q: Optional[str] = None):
 # Health check endpoint
 @app.get("/health")
 async def health_check():
+    logger.info('health /')
     return {
         "status": "healthy" if embeddings and llm else "unhealthy",
         "embeddings_initialized": embeddings is not None,
